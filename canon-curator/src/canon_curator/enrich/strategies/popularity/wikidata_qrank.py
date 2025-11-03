c49022fe519edc49f556386ea9823af0a663907e
Lisa Poggel <poggellisa@gmail.com>
2025-11-03 10:33:08 +0100

    feat(enrich): scaffold popularity strategies

---
+++ canon-curator/src/canon_curator/enrich/strategies/popularity/wikidata_qrank.py
@@ -0,0 +1,6 @@
+from canon_curator.models.enrichment import PopularityRecord
+from canon_curator.models.records import BaseWorkRecord
+
+
+def wikidata_qrank(record: BaseWorkRecord) -> list[PopularityRecord]:
+    pass
