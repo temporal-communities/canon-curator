1e92ec05d08d6f91cfc0fed6abea557e7aba77dc
Lisa Poggel <poggellisa@gmail.com>
2026-03-24 18:11:10 +0100

    chore(enrich): add QRankClient to __init__

--- src/canon_curator/enrich/clients/__init__.py
+++ src/canon_curator/enrich/clients/__init__.py
@@ -1,5 +1,6 @@
 from .gnd_client import GNDClient
 from .wikidata_client import WikidataClient
 from .goodreads_client import GoodreadsClient
+from .qrank_client import QRankClient
 
-__all__ = ["GNDClient", "WikidataClient", "GoodreadsClient"]
+__all__ = ["GNDClient", "WikidataClient", "GoodreadsClient", "QRankClient"]
