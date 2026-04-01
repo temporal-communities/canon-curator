1a94a9f1af0fb1cbcf9e238e75409cd4a32a05e2
Lisa Poggel <poggellisa@gmail.com>
2026-04-01 14:46:34 +0200

    chore(enrich): add GNDProperties to __init__

--- src/canon_curator/enrich/clients/__init__.py
+++ src/canon_curator/enrich/clients/__init__.py
@@ -1,6 +1,6 @@
-from .gnd_client import GNDClient
+from .gnd_client import GNDClient, GNDProperties
 from .wikidata_client import WikidataClient
 from .goodreads_client import GoodreadsClient
 from .qrank_client import QRankClient
 
-__all__ = ["GNDClient", "WikidataClient", "GoodreadsClient", "QRankClient"]
+__all__ = ["GNDClient", "GNDProperties", "WikidataClient", "GoodreadsClient", "QRankClient"]
