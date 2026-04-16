8cf3a0434f1f9f631708a76dbf68b740a859d536
Lisa Poggel <poggellisa@gmail.com>
2026-04-16 12:32:03 +0200

    test(load): add tests and test data for JSONLinesExporter

---
+++ tests/test_load.py
@@ -0,0 +1,19 @@
+import pytest
+from pathlib import Path
+from io import StringIO
+
+from canon_curator.load import JSONLinesExporter
+
+
+EXPECTED_JSONL_PATH = Path(__file__).parent / "testdata" / "expected_jsonlines_export.jsonl"
+EXPECTED_JSONL = EXPECTED_JSONL_PATH.read_text(encoding="utf-8")
+
+
+def test_jsonlies_export_success(
+	expected_enriched_work_record, expected_empty_enriched_work_record
+):
+	buffer = StringIO()
+	exporter = JSONLinesExporter("test.jsonl")
+	exporter.file = buffer
+	exporter.export([expected_enriched_work_record, expected_empty_enriched_work_record])
+	assert buffer.getvalue() == EXPECTED_JSONL
